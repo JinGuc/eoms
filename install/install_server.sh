8a69796b0a9f3a51bad411829834f216bf968ba0
xinnian.du <22816893@qq.com>
2022-12-14 14:08:12 +0800

    端口加入iptables

--- install/install_server.sh
+++ install/install_server.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 cur_dir=$(pwd)
 if [ "$1" = "www" ]; then
-    ${cur_dir}/lamp.sh --apache_option 2 --db_option 3 --php_option 2 --supervisord_option 1 --www_option 1 --only_install_www_option 1 --snmp_option 1 --pyeoms_option 1
+    ${cur_dir}/lamp.sh --apache_option 2 --db_option 3 --php_option 2 --supervisord_option 1 --only_install_www_option 1
 else
-    ${cur_dir}/lamp.sh --apache_option 1 --db_option 1 --php_option 1 --supervisord_option 1 --www_option 1 --snmp_option 1 --pyeoms_option 1 --db_manage_modules phpmyadmin
+    ${cur_dir}/lamp.sh --apache_option 1 --db_option 1 --php_option 1 --supervisord_option 1 --www_option 1 --db_manage_modules phpmyadmin
 fi
