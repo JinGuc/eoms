b66c2ca39854996d1fc58ce75a2fe217bf017c10
fenghao <970599508@qq.com>
2023-01-06 23:19:58 +0800

    移动端新增设备数据监控并刷新数据

--- www/database/migrations/2022_12_01_164654_update_url_info20221201.php
+++ www/database/migrations/2022_12_01_164654_update_url_info20221201.php
@@ -14,13 +14,13 @@ class UpdateUrlInfo20221201 extends Migration
     public function up()
     {
         Schema::table('url_info', function (Blueprint $table) {
-            if(!Schema::hasColumn('url_info','username'))
+            if(!Schema::hasColumn('url_info','db_username'))
             {
-                $table->string('username','50')->nullable()->default('')->comment('数据库账号');  
+                $table->string('db_username','50')->nullable()->default('')->comment('数据库账号');  
             }
-            if(!Schema::hasColumn('url_info','password'))
+            if(!Schema::hasColumn('url_info','db_password'))
             {
-                $table->string('password','50')->nullable()->default('')->comment('数据库密码');  
+                $table->string('db_password','50')->nullable()->default('')->comment('数据库密码');  
             }
         });
     }
